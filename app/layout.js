6fc1e849dfcbb1b3185b0cd881118520c776a1c7
Harry Parkinson <71083434+harryparkinson101@users.noreply.github.com>
2024-08-06 07:19:26 +0100

    commented out footer for now

--- app/layout.js
+++ app/layout.js
@@ -1,28 +1,27 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/Navabar";
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Your Dream Saas",
-  description: "SaaS Web Developement Agency",
+  description: "SaaS Web Development Agency",
 };
 
-export default function RootLayout({ children, className = "" }) {
+const RootLayout = ({ children, className = "" }) => {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div
-          className={`z-0 inline-block h-full w-full bg-light p-0 dark:bg-dark xl:p-24 lg:p-16 
-      md:p-12 sm:p-8 ${className}`}
-        >
+        <div className={`z-0 inline-block h-full w-full bg-light p-0 dark:bg-dark xl:p-24 lg:p-16 md:p-12 sm:p-8 ${className}`}>
           <Navbar />
           {children}
-          <Footer />
+          {/* <Footer /> */}
         </div>
       </body>
     </html>
   );
-}
+};
+
+export default RootLayout;
