374af25542dbc12b94909ef4c977b2bc401e07fc
Harry Parkinson <71083434+harryparkinson101@users.noreply.github.com>
2024-08-10 18:32:52 +0100

    made CloseIcon a componet so dont export 2 componets in one file

--- app/(routes)/projects/page.tsx
+++ app/(routes)/projects/page.tsx
@@ -12,7 +12,8 @@ import { useOutsideClick } from "../../hooks/useOutsideClick";
 import { HeroParallax } from "../../components/ui/hero-parallax";
 import { AuroraBackground } from "../../components/ui/aurora-background";
 import { CardBody, CardContainer, CardItem } from "../../components/ui/3d-card";
-import Link from "next/link";
+import { CloseIcon } from "../../components/CloseIcon"; // Adjust the path accordingly
+
 
 export default function Projects() {
   const [active, setActive] = useState<(typeof cards)[number] | boolean | null>(null);
@@ -153,30 +154,6 @@ export default function Projects() {
     </AuroraBackground>
   );
 }
-
-export const CloseIcon = () => {
-  return (
-    <motion.svg
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0, transition: { duration: 0.05 } }}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="h-4 w-4 text-black"
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <path d="M18 6l-12 12" />
-      <path d="M6 6l12 12" />
-    </motion.svg>
-  );
-};  
 
 const cards = [
   {
