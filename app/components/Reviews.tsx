5f4bdf9c5adbb9c67965f826b092e83a5e8ac8bd
Harry Parkinson <71083434+harryparkinson101@users.noreply.github.com>
2024-08-21 23:08:54 +0100

    added classNames to Reviews incase i need it inside another component

--- app/components/Reviews.tsx
+++ app/components/Reviews.tsx
@@ -1,9 +1,10 @@
-"use client"
+"use client";
 import { HoverEffect } from "./ui/card-hover-effect";
 import { reviews } from "../utils/data";
-export default function Reviews() {
+
+export default function Reviews({ className }: { className?: string }) {
   return (
-    <div className="max-w-5xl mx-auto px-2 sm:px-0">
+    <div className={`max-w-5xl mx-auto px-2 sm:px-0 ${className}`}>
       <HoverEffect items={reviews} />
     </div>
   );
